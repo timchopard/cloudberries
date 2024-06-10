a7de9087a28d897e511596dad2718b5d08c8376f
tim <timothyjchandler@gmail.com>
2024-06-10 10:58:51 +0200

    updated csrf settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,3 +138,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://cloudberries.io",
+    "https://www.cloudberries.io",
+]
